65cbec74275eeb216fed9fd07307d49eb81f08e6
liang799 <71926348+liang799@users.noreply.github.com>
2024-08-23 22:18:43 +0800

    Removed redundant code

--- test_e2e.py
+++ test_e2e.py
@@ -18,8 +18,6 @@ class LatLong:
 
 
 class TelegramBot:
-    state = 'idle'
-
     def __init__(self, bot: Application):
         self.passenger_lat_long = None
         self.driver_lat_long = None
@@ -28,22 +26,12 @@ class TelegramBot:
     def decides_that_passenger_destination_is_too_far(self):
         dist_km = self.driver_lat_long.compare_distance_km(self.passenger_lat_long)
         assert dist_km > 5.0
-        assert self.state == 'idle'
-
-    def should_not_inform_driver_about_potential_passenger(self):
-        assert self.state == 'idle'
-
-    def should_not_list_driver_in_passenger_waiting_screen(self):
-        assert self.state == 'idle'
 
     def saveDriver(self, lat_long: LatLong):
         self.driver_lat_long = lat_long
 
     def savePassenger(self, lat_long: LatLong):
         self.passenger_lat_long = lat_long
-
-    def setBotState(self, state: str):
-        self.state = state
 
 
 class User:
@@ -62,12 +50,10 @@ class User:
             assert resp.text in "Received location: Hougang Mall " \
                                 "@1.372455,103.8938277"
             bot.saveDriver(LatLong(1.372455, 103.8938277))
-            bot.setBotState("idle")
 
     async def send_drop_off_point_to(self, bot: TelegramBot):
         async with self.client.conversation("@gedong_ride_share_bot", timeout=5) as conv:
             await conv.send_message("/drop_off")
-            bot.setBotState("busy")
             resp = await conv.get_response()
             assert resp.text in "Please share the Google Map URL Location. Example: " \
                                 "https://maps.app.goo.gl/UuEC3fpGAHV9a7K38"
@@ -76,7 +62,6 @@ class User:
             assert resp.text in "Received location: West Coast Park " \
                                 "@1.3140476,103.7416346"
             bot.savePassenger(LatLong(1.3140476, 103.7416346))
-            bot.setBotState("idle")
 
 
 @pytest.mark.asyncio(scope="session")  # The asyncio event loop must not change after connection
@@ -89,4 +74,3 @@ async def test_no_match_because_passenger_drop_off_point_too_far(client: Telegra
     await passenger.send_drop_off_point_to(bot)
 
     bot.decides_that_passenger_destination_is_too_far()
-    bot.should_not_inform_driver_about_potential_passenger()
